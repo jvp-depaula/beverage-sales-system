7c01e22fab13c1430648f78773efd690c9e7afe0
Jair de Paula <jvp_paula@hotmail.com>
2023-11-28 23:38:18 -0300

    correção do último commit

--- Models/Parcelas.cs
+++ Models/Parcelas.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿namespace Sistema.Models
+namespace Sistema.Models
 {
     public class Parcelas
     {
@@ -10,32 +9,5 @@
         public double? nrParcela { get; set; }
         public string flSituacao { get; set; }
         public DateTime? dtPagamento { get; set; }
-=======
-﻿using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Xml.Linq;
-
-namespace Sistema.Models
-{
-    public class Parcelas
-    {
-        [Key]
-        [Column(Order = 0)]
-        public int idCondicaoPgto { get; set; }
-        [Key]
-        [Column(Order = 1)]
-        public int idFormaPgto { get; set; }
-        [Display(Name = "Dias")]
-        public string dias { get; set; }
-        [Display(Name = "% Parcela")]
-        public decimal txPercentParcela { get; set; }
-        [Display(Name = "% Juros")]
-        public decimal txPercentJuros { get; set; }
-        [Display(Name = "% Multa")]
-        public decimal txPercentMulta { get; set; }
-        [Display(Name = "% Desconto")]
-        public decimal txPercentDesconto { get; set; }
->>>>>>> e62caf04ce1315a201b6806efdf2275de71abc27
     }
 }
